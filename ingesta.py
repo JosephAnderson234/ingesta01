a05c46686d152cd0cce1e3b5259703cbb5901f52
JosephAnderson234 <joseph.cose@utec.edu.pe>
2025-09-24 07:25:56 -0500

    Actualizar el nombre del bucket y la ruta de carga en el script de ingesta de datos

--- ingesta.py
+++ ingesta.py
@@ -1,10 +1,10 @@
 import boto3
 
 ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+nombreBucket = "joseph-s2-data"
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+response = s3.upload_file(ficheroUpload, nombreBucket, "ingesta/" + ficheroUpload)
 print(response)
 
 print("Ingesta completada")
